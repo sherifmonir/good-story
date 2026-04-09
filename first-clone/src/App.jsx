63b20b9e84e95d11ae5f37d17da9cc9402eee57d
Sherif Monir <sherif.monir.badreddin.com>
2026-04-09 11:50:42 +0200

    fix: initialize count state to 1

--- first-clone/src/App.jsx
+++ first-clone/src/App.jsx
@@ -5,7 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>

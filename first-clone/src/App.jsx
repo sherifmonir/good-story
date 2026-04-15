72e8ac9a036140094a7e7c039a6cac159b9a3b23
Sherif Monir <sherif.monir.badreddin@gmail.com>
2026-04-15 14:52:57 +0200

    fix: refactor Card component to manage like state and update count display

--- first-clone/src/App.jsx
+++ first-clone/src/App.jsx
@@ -1,15 +1,27 @@
 
 import './App.css'
+import { useState, useEffect } from 'react'
 
 function Card({title}){
+    const [count, setCount] = useState(0)
+    const [hasLiked, setHasLiked] = useState(false)
+
+    useEffect(() => {
+      console.log(`${title} has been liked: ${hasLiked}`)
+    }, [title,hasLiked]);
+
   return (
-    <div className='card'>
-      <h2>{title}</h2>
+    <div className='card' onClick={() => setCount((prevCount) => prevCount + 1) }>
+      <h2>{title} - {count}<br/>  </h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️' : '🤍'}
+      </button>
     </div>
   )
 }
 
 function App()  {
+
   return (
     <div className='card-container'>
 
